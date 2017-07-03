ec56c8eb5968e0f0a3ec33fe88ad8abee7dc166d
poonsung <cmp.poon@gmail.com>
2017-07-03 21:43:38 +0700

    add samples by poon

---
+++ samples/data_processing/cluster/create_cluster.php
@@ -0,0 +1,40 @@
+#!/usr/bin/env php
+<?php
+ 
+require 'vendor/autoload.php';
+ 
+use OpenStack\OpenStack;
+ 
+$openstack = new OpenStack([
+    'authUrl' => '{authUrl}',
+    'user'    => [
+        'name'       => '{userName}',
+        'password' => '{password}',
+        'domain' => [ 'name' => '{userDomain}' ]
+    ],
+    'scope'   => [
+        'project' => [
+             'name' => '{projectName}',
+             'domain' => [ 'name' => '{projectDomain}' ]
+        ]
+    ]
+]);
+ 
+$sahara = $openstack->dataProcessingV1(['region' => '{region}']);
+
+$options = [
+		'multiple'		 => '{createMultipleOrnot}',
+		'pluginName'     => '{pluginName}',
+		'hadoopVersion'  => '{hadoopVersion}',
+		'clusterTemplateId' => '{clusterTemplateId}',
+		'defaultImageId' => '{defaultImageId}',
+		//user keypair id is optional
+		'userKeypairId'  => '{userKeypairId}',
+		'name' => '{name}',
+		'neutronManagementNetwork' => '{neutronManagementNetwork}'
+];
+
+$cluster = $sahara->createCluster($options);
+
+?>
+
