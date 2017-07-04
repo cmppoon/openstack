02db8d19242eba6be84799b0ea08a949ab3bdbb1
poonsung <cmp.poon@gmail.com>
2017-07-04 10:19:33 +0700

    update samles by poon

---
+++ samples/data_processing/datasource/delete_datasource.php
@@ -0,0 +1,30 @@
+#!/usr/bin/env php
+<?php
+ 
+require 'vendor/autoload.php';
+ 
+use OpenStack\OpenStack;
+ 
+$openstack = new OpenStack([
+		'authUrl' => '{authUrl}',
+		'user'    => [
+				'name'       => '{userName}',
+				'password' => '{password}',
+				'domain' => [ 'name' => '{userDomain}' ]
+		],
+		'scope'   => [
+				'project' => [
+						'name' => '{projectName}',
+						'domain' => [ 'name' => '{projectDomain}' ]
+				]
+		]
+]);
+
+$sahara = $openstack->dataProcessingV1(['region' => '{region}']);
+
+$source = $sahara->getDataSource(['id' => '{dataSourceId}']);
+
+$source->delete();
+
+?>
+
