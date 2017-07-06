9fb99acc312aa000ead60bf7705844407b6925cd
onishield <onishield@gmail.com>
2017-07-06 10:49:46 +0700

    Image variable has created.

--- src/DataProcessing/v1/Models/Image.php
+++ src/DataProcessing/v1/Models/Image.php
@@ -10,5 +10,21 @@ use OpenStack\Common\Resource\Retrievable;
 
 class Image extends OperatorResource implements Listable, Retrievable, Creatable, Deletable
 {
+  public $status;
+  public $username;
+  public $updated;
+  public $OSEXTIMGSIZEsize;
+  public $created;
+  public $tags;
+  public $minDisk;
+  public $name;
+  public $progress;
+  public $minRam;
+  public $metadata;
+  public $id;
+  public $description;
 
+  protected $aliases = [
+			'OS-EXT-IMG-SIZE:size'					=>	'OSEXTIMGSIZEsize'
+	];
 }
